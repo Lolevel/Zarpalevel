2b39398824c6e09fa3f8f1a8e6e07936b52fd188
Lolevel <levi.amon.strube@gmail.com>
2021-09-18 18:57:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React please
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="discord.gg/4VvXqxaHaR"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Join my discord server
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
