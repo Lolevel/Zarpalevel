22f9e786a79d81a10fb5991867b4378452e64c94
Lolevel <levi.amon.strube@gmail.com>
2021-09-18 18:59:41 +0200

    fixed link to dc server

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
                 </p>
                 <a
                     className="App-link"
-                    href="discord.gg/4VvXqxaHaR"
+                    href="https://discord.gg/4VvXqxaHaR"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
